382dc2ca7faa735a86611d0f0d7c4510643e8477
Ben Pinali <ben@benpinali.com>
2016-02-01 20:33:57 -0600

    Added jQuery file, changed js to output appended rows to html

--- js/fizzbuzzrefactor.js
+++ js/fizzbuzzrefactor.js
@@ -1,11 +1,30 @@
-for (var counter = 1; counter <= 100; counter++) {
-    if (counter % 15 == 0) {
-        console.log("FizzBuzz");
-    } else if (counter % 3 == 0) {
-        console.log("Fizz");
-    } else if (counter % 5 == 0) {
-        console.log("Buzz");
-    } else {
-        console.log(counter);
+$(document).ready(function () {
+
+    function play(max) {
+
+        for (var counter = 1; counter <= max; counter++) {
+            if (counter % 15 == 0) {
+                var row = "<li>FizzBuzz</li>";
+            } else if (counter % 3 == 0) {
+                var row = "<li>Fizz</li>";
+            } else if (counter % 5 == 0) {
+                var row = "<li>Buzz</li>";
+            } else {
+                var row = "<li>" + counter + "</li>";
+            }
+            $('#results').append(row);
+        }
     }
-}
+
+    $('#calculate').on('click', function () {
+        var maxNumber = parseInt($('#fizzbuzzinput').val());
+        // console.log(maxNumber);
+        //validate here
+        if (isNaN(maxNumber)) {
+            alert('Please add a number greater than zero.')
+        } else {
+            play(maxNumber);
+        }
+
+    });
+});
